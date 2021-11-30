d1592da25422a8f4da86447e46f1aafa9b2c74d6
Talles Mello <tallesmello@gmail.com>
2021-11-30 19:31:22 -0300

    criação de link telrefone, sms, map, e activity furmulario para imagem e uso de camera.

--- app/src/main/java/com/example/agendaalura/ListaAlunoActivity.java
+++ app/src/main/java/com/example/agendaalura/ListaAlunoActivity.java
@@ -1,8 +1,11 @@
 package com.example.agendaalura;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Browser;
@@ -43,12 +46,13 @@ public class ListaAlunoActivity extends AppCompatActivity {
 
         FloatingActionButton novoAluno = findViewById(R.id.lista_alunos_btn_novo_aluno);
         novoAluno.setOnClickListener((v) -> {
-                Intent btnFormulario = new Intent(ListaAlunoActivity.this, FormularioActivity.class);
-                startActivity(btnFormulario);
-            });
+            Intent btnFormulario = new Intent(ListaAlunoActivity.this, FormularioActivity.class);
+            startActivity(btnFormulario);
+        });
 
         registerForContextMenu(listaAlunos);
     }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -70,17 +74,44 @@ public class ListaAlunoActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, final ContextMenu.ContextMenuInfo menuInfo)  {
+    public void onCreateContextMenu(ContextMenu menu, View v, final ContextMenu.ContextMenuInfo menuInfo) {
 
         AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
         final Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
+
+        MenuItem itemLigar = menu.add("Ligar");
+        itemLigar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (ActivityCompat.checkSelfPermission(ListaAlunoActivity.this, Manifest.permission.CALL_PHONE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(ListaAlunoActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 123);
+                } else {
+                    Intent intentLigar = new Intent(Intent.ACTION_CALL);
+                    intentLigar.setData(Uri.parse("tel:" + aluno.getTelefone()));
+                    startActivity(intentLigar);
+                }
+                return false;
+            }
+        });
+
+        MenuItem itemSms = menu.add("enviar SMS");
+        Intent intentSms = new Intent(Intent.ACTION_VIEW);
+        intentSms.setData(Uri.parse("sms:" + aluno.getTelefone()));
+        itemSms.setIntent(intentSms);
+
+        MenuItem itemMap = menu.add("Mapa");
+        Intent intentMap = new Intent(Intent.ACTION_VIEW);
+        intentMap.setData(Uri.parse("geo:0,0?q=" + aluno.getEndereco()));
+        itemMap.setIntent(intentMap);
+
 // click long + abrir um site direto do app
         MenuItem itemSite = menu.add("visitar site");
         Intent intentSite = new Intent(Intent.ACTION_VIEW);
 
         String site = aluno.getSite();
         if (!site.startsWith("http://")) {
-            site ="http://" + site;
+            site = "http://" + site;
         }
 
         intentSite.setData(Uri.parse(site));
